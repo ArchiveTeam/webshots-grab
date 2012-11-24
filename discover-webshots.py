37322ee7bb3d0cf09675e47e9de4adb580515a0d
Alard <alard@example.com>
2012-11-24 17:20:50 +0100

    Search for multiple prefixes.

--- discover-webshots.py
+++ discover-webshots.py
@@ -14,55 +14,62 @@ USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20
 http_client = httpclient.HTTPClient()
 
 print "Loading task..."
-res = http_client.fetch("http://tracker.archiveteam.org:8123/request-discover", method="POST", body="")
+res = http_client.fetch("http://tracker.archiveteam.org:8123/request-discover", method="POST", body="n=25")
 task = json.loads(res.body)
 # task = { "id": 123, "prefix": "ab03" }
 
 print "Task %s" % task["id"]
-prefix = task["prefix"]
+prefixes = task["prefixes"]
 
-if prefix == None:
+if prefixes == []:
   print "No task."
   exit()
 
 
 usernames = []
-count = 0
+next_prefixes = []
 
-while True:
-  query = urllib.quote(prefix.ljust(3, "%") + "%")
-  req = httpclient.HTTPRequest(
-      ("http://www.webshots.com/explore/member?action=userSearch&username=%s" % query),
-      connect_timeout=10, request_timeout=30,
-      user_agent=USER_AGENT)
-  try:
-    res = http_client.fetch(req)
-    matches = re.findall('http:\/\/community\.webshots\.com\/user\/([^/]+)', res.body)
-    usernames = [value for value in matches if value != "my"]
-    count = res.body.count("http://community.webshots.com/user/")
-    break
+for prefix in prefixes:
+  count = 0
 
-  except httpclient.HTTPError, e:
-    print "Error:", e
-    if e.code == 599:
-      # timeout?
-      time.sleep(10)
-    else:
-      # not found
+  print " - Prefix: '%s'" % prefix
+  while True:
+    query = urllib.quote(prefix.ljust(3, "%") + "%")
+    req = httpclient.HTTPRequest(
+        ("http://www.webshots.com/explore/member?action=userSearch&username=%s" % query),
+        connect_timeout=10, request_timeout=30,
+        user_agent=USER_AGENT)
+    try:
+      res = http_client.fetch(req)
+      matches = re.findall('http:\/\/community\.webshots\.com\/user\/([^/]+)', res.body)
+      for value in matches:
+        if value != "my":
+          usernames.append(value)
+      count = res.body.count("http://community.webshots.com/user/")
       break
 
-if count < 100:
-  # End of this query reached
-  next_prefixes = []
-else:
-  # Not specific enough, we need to go a level deeper
-  next_prefixes = ["%s%s" % (prefix, c) for c in alphabet]
+    except httpclient.HTTPError, e:
+      print "Error:", e
+      if e.code == 599:
+        # timeout?
+        time.sleep(10)
+      else:
+        # not found
+        break
+
+  if count < 100:
+    # End of this query reached
+    next_prefixes = []
+  else:
+    # Not specific enough, we need to go a level deeper
+    for c in alphabet:
+      next_prefixes.append("%s%s" % (prefix, c))
 
 
 print
 print "Submitting results (%d usernames, %d new prefixes)..." % (len(usernames), len(next_prefixes))
-json_body = json.dumps({ "id": task["id"], "prefix": prefix, "usernames": usernames, "next_prefixes": next_prefixes })
-print json_body
+json_body = json.dumps({ "id": task["id"], "prefixes": prefixes, "usernames": usernames, "next_prefixes": next_prefixes })
+# print json_body
 res = http_client.fetch("http://tracker.archiveteam.org:8123/submit-discover", method="POST",
                         body=json_body, headers={"Content-Type": "application/json"})
 
